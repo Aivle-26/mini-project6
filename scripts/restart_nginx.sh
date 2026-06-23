3e82ed5117dff2e64225af1ca6c002e8a50c6745
kim <minsic2000@gmail.com>
2026-06-23 17:08:15 +0900

    Align CodeDeploy destination with nginx root

--- scripts/restart_nginx.sh
+++ scripts/restart_nginx.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+mkdir -p /var/www/my-angular-project
+chown -R nginx:nginx /var/www/my-angular-project || true
+chmod -R 755 /var/www/my-angular-project
+nginx -t
 systemctl restart nginx
